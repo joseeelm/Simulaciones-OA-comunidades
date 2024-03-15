84a980983f6bb26058bc524a72444f66d80c5dc9
José López M <53451847+joseeelm@users.noreply.github.com>
2024-03-15 10:33:22 -0300

    Update Simulaciones OA comunidades multiproceso.py

--- Simulaciones OA comunidades multiproceso.py
+++ Simulaciones OA comunidades multiproceso.py
@@ -7,11 +7,11 @@ import numpy as np
 import Ott_Antonsen_comunidades_v3_GITHUB as OAC
 
 # Valores de x0 para la interaccion ENTRE comunidades:
-vec_x0_3 = np.array([-0.04, 0, 0.04, 0.08])
+vec_x0_3 = np.array([-0.08, -0.04, -0.02, 0, 0.02, 0.04, 0.08])
 
 # Ejecucion de las simulaciones para varios x0:
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(processes=4)
+    pool = multiprocessing.Pool(processes=7)
     pool.map(OAC.ejecutar, vec_x0_3)
 
 
